2f0fd3fec8aa92a2cfd3a7d9e840517399851ba3
Wina-ui <dhwangdepaul@gmail.com>
2025-06-17 13:16:36 -0500

    f

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import { useState } from 'react';
-
+import './App.css';
 function App() {
   const [count, setCount] = useState(0);
+  function getRedGradient(count) {
+    const shift = (count * 15) % 360;
+    const color1 = `hsl(${shift}, 100%, 50%)`;
+    const color2 = `hsl(${(shift + 20) % 360}, 100%, 40%)`;
+    const color3 = `hsl(${(shift + 40) % 360}, 100%, 30%)`;
+
+    return {
+      '--color1': color1,
+      '--color2': color2,
+      '--color3': color3
+    };
+  }
 
   return (
-    <div style={{ textAlign: 'center', marginTop: '100px' }}>
+    <div classname ="container" style={{ textAlign: 'center', marginTop: '100px' }}>
       <h1>Simple React Counter</h1>
       <p>You clicked {count} times</p>
-      <button onClick={() => setCount(count + 1)}>
+      <button className = 'rainbow-button' style={getRedGradient(count)} onClick={() => setCount(count + 1)}>
         Click Me
       </button>
     </div>
